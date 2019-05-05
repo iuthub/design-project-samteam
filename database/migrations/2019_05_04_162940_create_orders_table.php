bc171c6f33ddc67dfe0c20bfe9817242d4ccb71a
DurovaYu <yulianna199820@gmail.com>
2019-05-06 02:52:53 +0500

    products sessions orders #8

---
+++ database/migrations/2019_05_04_162940_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer('user_id');
+            $table->text('cart');
+            $table->text('address');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('payment_id');
+            $table->string('phone, 12')->nullable();
+            $table->string('code')->nullable();
+            $table->boolean('active')->default(0);
+            $table->rememberToken();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
